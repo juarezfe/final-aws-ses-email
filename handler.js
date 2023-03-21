c957d3b2e3617575c73725dffa7f5a0971b50bf4
Jose Juarez <jose.juarez@levadata.com>
2023-03-21 12:58:54 -0600

    initial commit AWS SES Email

---
+++ handler.js
@@ -0,0 +1,50 @@
+
+const AWS = require("aws-sdk")
+const ses = new AWS.SES()
+
+module.exports.createContact = async (event, context) => {
+  console.log('====================================');
+  console.log("Received:::", event);
+  console.log('====================================');
+  const { to, from, subject, message } = JSON.parse(event.body)
+
+  if (!to || !from || !subject || !message) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: " to or from... are not set properly!" })
+    }
+  }
+  const params = {
+    Destination: {
+      ToAddresses: [to]
+    },
+    Message: {
+      Body: {
+        Text: { Data: message }
+      },
+      Subject: { Data: subject }
+    },
+    Source: from
+  }
+  try {
+    await ses.sendEmail(params).promise()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "email sent succesfully!",
+        success: true
+      })
+    }
+
+  } catch (error) {
+    console.error(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "The email failed to send",
+        success: true
+      })
+    }
+  }
+};
+ 
